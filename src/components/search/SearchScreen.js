66aafd23cb30c9661105dd423553c3f916794a8c
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-09 18:15:46 -0500

    SearchComponent usando un hook

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 export const SearchScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    searchText: "",
+  });
+
+  const { searchText } = formValues;
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>SearchScreen</h1>
-    </div>
+    <>
+      <h1>Búsquedas</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <h4>Buscar</h4>
+          <hr />
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Buscar un héroe"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={searchText}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              Buscar...
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
