93c07713cc9b779200514d857fd2ad3047e67ce2
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-09 17:19:25 -0500

    Leer argumentos por URL y estilos del componente HeroScreen

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,9 +1,57 @@
 import React from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+  const { heroeId } = useParams();
+
+  const navigate = useNavigate();
+
+  const hero = getHeroById(heroeId);
+
+  const handleReturn = () => {
+    navigate(-1);
+  };
+
+  if (!hero) {
+    return <Navigate to="/" />;
+  }
+
+  const {
+    id,
+    superhero,
+    publisher,
+    alter_ego,
+    first_appearance,
+    characters,
+  } = hero;
+
+  const imagePath = `/assets/${id}.jpg`;
+
   return (
-    <div>
-      <h1>Hero</h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={imagePath} alt={hero.superhero} className="img-thumbnail" />
+      </div>
+      <div className="col8">
+        <h3>{superhero}</h3>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First Appearance:</b> {first_appearance}
+          </li>
+        </ul>
+        <h5 className="mt-3">Characters</h5>
+        <p>{characters}</p>
+        <button className="btn btn-outline-info" onClick={handleReturn}>
+          Regresar
+        </button>
+      </div>
     </div>
   );
 };
