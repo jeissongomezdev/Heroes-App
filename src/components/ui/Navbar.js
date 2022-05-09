a2510daf77879826bf726dbddbd8aa910e641974
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-09 11:50:35 -0500

    Creando un segundo Router

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 
 export const Navbar = () => {
+  const handleLogout = () => {
+    //TODO: Por hacer
+    console.log("Logout");
+  };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/">
@@ -10,21 +15,32 @@ export const Navbar = () => {
 
       <div className="navbar-collapse">
         <div className="navbar-nav">
-          <NavLink className="nav-item nav-link" to="/marvel">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link" + (isActive ? "active" : "")
+            }
+            to="/marvel"
+          >
             Marvel
           </NavLink>
 
-          <NavLink className="nav-item nav-link" to="/dc">
+          <NavLink
+            className={({ isActive }) =>
+              "nav-item nav-link" + (isActive ? "active" : "")
+            }
+            to="/dc"
+          >
             DC
           </NavLink>
         </div>
       </div>
 
-      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+      <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <NavLink className="nav-item nav-link" to="/login">
+          <span className="nav-item nav-link text-info">Jeisson</span>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>
             Logout
-          </NavLink>
+          </button>
         </ul>
       </div>
     </nav>
