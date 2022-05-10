fa2abf89a1f24cefa52546a4a8450b00cd57d181
Jeisson Gómez <jeissongomez@hotmail.es>
2022-05-10 12:27:44 -0500

    Proteccion de rutas, Context y Reducer, proveer estado global de la aplicacion, login

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,5 +1,29 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import { AuthContext } from "./auth/authContext";
+import { authReducer } from "./auth/authReducer";
 import { AppRouter } from "./routers/AppRouter";
+
+const init = () => {
+  return localStorage.getItem("user") || { logged: false };
+};
+
 export const HeroesApp = () => {
-  return <AppRouter />;
+  const [user, dispatch] = useReducer(authReducer, {}, init);
+
+  useEffect(() => {
+    if (!user) return;
+
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        dispatch,
+      }}
+    >
+      <AppRouter />
+    </AuthContext.Provider>
+  );
 };
